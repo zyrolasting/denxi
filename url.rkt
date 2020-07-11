44ace5a96e3f404c337d389d0cb95d0820b2e0aa
Sage Gerard <sage@sagegerard.com>
2020-07-11 01:37:49 -0400

    Rename file-directory-path to file-url->local-path

--- url.rkt
+++ url.rkt
@@ -8,7 +8,7 @@
 (provide (all-from-out net/url)
          (contract-out
           [indicates-fs-path? (-> url? boolean?)]
-          [find-directory-path (-> url? (or/c #f path?))]
+          [file-url->local-path (-> url? (or/c #f path?))]
           [url-string? predicate/c]))
 
 (define (url-string? s)
@@ -16,22 +16,17 @@
     (and (string->url s)
          #t)))
 
-(define (find-directory-path u)
+(define (file-url->local-path u)
   (with-handlers ([exn? (const #f)])
-    (define scheme (url-scheme u))
-    (and (or (equal? scheme "file")
-             (not scheme))
-         (indicates-fs-path? u)
-         (simplify-path
-          (apply build-path
-                 (if (url-path-absolute? u)
-                     (car (filesystem-root-list))
-                     (current-directory))
-                 (filter-map (λ (pp)
-                               (and (not (equal? "" (path/param-path pp)))
-                                    (path/param-path pp)))
-                             (url-path u)))))))
-
+    (simplify-path
+     (apply build-path
+            (if (url-path-absolute? u)
+                (car (filesystem-root-list))
+                (current-directory))
+            (filter-map (λ (pp)
+                          (and (not (equal? "" (path/param-path pp)))
+                               (path/param-path pp)))
+                        (url-path u))))))
 
 (define (indicates-fs-path? u)
   (define leading (get-leading-path-element u))
