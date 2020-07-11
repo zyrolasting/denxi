c07f81219d5d68fd89795657f62b9ecd379d4b3e
Sage Gerard <sage@sagegerard.com>
2020-07-11 19:32:21 -0400

    Add missing URL bindings to server.rkt

--- service/server.rkt
+++ service/server.rkt
@@ -14,6 +14,7 @@
                     web-server/dispatchers/dispatch-lift)
          "../workspace.rkt"
          "../config.rkt"
+         "../url.rkt"
          "catalog.rkt"
          "endpoint.rkt")
 
