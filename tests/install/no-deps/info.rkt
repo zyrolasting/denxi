ddd3108f4c9fc98744b64878a658c404cc660f26
Sage Gerard <sage@sagegerard.com>
2020-07-11 19:33:04 -0400

    Remove outdated install test

--- tests/install/no-deps/info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-
-(define provider-name "you")
-(define package-name "no-deps")
-(define edition-name "draft")
-(define revision-number 0)
-(define revision-names '("initial"))
-(define installer "installer.rkt")
-(define dependencies '())
--- tests/install/no-deps/installer.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket/base
-
-(define (set-up!)
-  (displayln "Setting up"))
-
-(define (tear-down!)
-  (void))
