93391b991380299262825b9934313babbaecb744
Sage Gerard <sage@sagegerard.com>
2020-07-11 18:00:04 -0400

    Add web-server-lib dep and shorten description

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 
 (define collection "zcpkg")
-(define description "An alternative package manager for Racket, built from lessons learned from PLaneT and raco pkg.")
-(define deps '("base"))
+(define description "A better package manager for Racket")
+(define deps '("base" "web-server-lib"))
 (define raco-commands '(("zcpkg" (submod zcpkg/client/main main) "Manage zero-collection packages" #f)))
 (define scribblings '(("scribblings/zcpkg.scrbl" ())))
 (define version "0.0")
