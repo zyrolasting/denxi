0399fcac3f78fedca381dc52d2c5dea182f7d8a0
Sage Gerard <sage@sagegerard.com>
2020-07-14 14:12:21 -0400

    Add tests and fix initial bugs in metadata.rkt refactor

--- metadata.rkt
+++ metadata.rkt
@@ -23,21 +23,25 @@
 
 (define (get-metadatum #:optional? [optional? #f] cnt path)
   (define lockfile (make-lock-file-name path))
-  (with-handlers ([exn?
-                   (λ (e)
-                     (delete-file lockfile)
-                     (cond [(exn:fail:contract? e)
-                            (raise (rewrite-contract-error-message e (file-name-from-path path)))]
-                           [(exn:fail:filesystem? e)
-                            (unless optional? (raise e))]
-                           [else (raise e)]))])
-    (call-with-file-lock/timeout
-     path 'shared
-     (λ () (invariant-assertion cnt (call-with-input-file path read)))
-     (λ () (error 'get-metadatum!
-                  "Failed to obtain lock for ~a"
-                  path)))
-    (delete-file lockfile)))
+  (dynamic-wind
+    void
+    (λ ()
+      (with-handlers ([exn?
+                       (λ (e)
+                         (cond [(exn:fail:contract? e)
+                                (raise (rewrite-contract-error-message e (file-name-from-path path)))]
+                               [(exn:fail:filesystem? e)
+                                (if optional? #f (raise e))]
+                               [else (raise e)]))])
+        (call-with-file-lock/timeout
+         path 'shared
+         (λ () (invariant-assertion cnt (call-with-input-file path read)))
+         (λ () (error 'get-metadatum!
+                      "Failed to obtain lock for ~a"
+                      path)))))
+    (λ ()
+      (when (file-exists? lockfile)
+        (delete-file lockfile)))))
 
 
 (define (set-metadatum! #:preamble [preamble null] cnt value path)
@@ -49,8 +53,8 @@
      (call-with-output-file #:exists 'truncate/replace
        path (λ (o)
               (for ([line (in-list preamble)])
-                (display "; ")
-                (displayln line))
+                (display "; " o)
+                (displayln line o))
               (write value o))))
    (λ () (error 'set-metadatum!
                 "Failed to obtain lock for ~a"
@@ -68,3 +72,42 @@
   (for ([requirement (in-list requirements)])
     (match-define (list cnt val filename) requirement)
     (set-metadatum! cnt val (build-path dir filename))))
+
+(module+ test
+  (require racket/file
+           racket/port
+           rackunit)
+
+  (define tmp-file (make-temporary-file))
+
+  (define (save-real v)
+    (set-metadatum! #:preamble (list "a comment" "another comment")
+                    real? v tmp-file))
+
+  (dynamic-wind
+    void
+    (λ ()
+      (save-real 10)
+      (test-eq? "Exchange metadatum with disk"
+                (get-metadatum real? tmp-file)
+                10)
+
+      (test-exn "Signal contract violations on read"
+                exn:fail:contract?
+                (λ () (get-metadatum string? tmp-file)))
+
+      (test-case "Protect file from invalid values"
+        (check-exn exn:fail:contract?
+                   (λ () (save-real "not real")))
+        (check-eq? (get-metadatum real? tmp-file)
+                   10))
+
+      (test-false "Optional metadatum is #f if file does not exist"
+                  (get-metadatum #:optional? #t real? "/var/nonselahdjdkfs"))
+
+      (test-exn "Optional metadatum must obey contract if file does exist"
+                exn:fail:contract?
+                (λ ()
+                  (get-metadatum #:optional? #t string? tmp-file))))
+
+    (λ () (delete-file tmp-file))))
