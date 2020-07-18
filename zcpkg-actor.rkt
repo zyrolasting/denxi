185d2e1d9fa5cc73795bfce216ec790c77df2ca5
Sage Gerard <sage@sagegerard.com>
2020-07-17 20:00:35 -0400

    Flesh out zcpkg-actor.rkt a bit

--- zcpkg-actor.rkt
+++ zcpkg-actor.rkt
@@ -5,6 +5,7 @@
 (require (only-in racket/generator in-generator yield)
          racket/contract
          racket/function
+         racket/list
          racket/exn
          racket/place
          racket/set
@@ -16,27 +17,50 @@
          "zcpkg-settings.rkt")
 
 
-; Flow control messages
-(define-message $start (config))
-(define-message $schedule (dependent dependencies))
-(define-message $frontlog (schedule))
-(define-message $backlog  (schedule))
-(define-message $crash (message))
-(define-message $done (message))
-(define-message $stop ())
 
-; Package management messages
+; Flow control messages
+(define-message $start    (workspace-directory))
+(define-message $frontlog (message))
+(define-message $backlog  (message))
+(define-message $fail     (to-display))
+(define-message $done     ())
+(define-message $stop     ())
+
+
+; Output messages
+(define-message $vertex (label))
+(define-message $edge (dependent dependency))
 (define-message $before-making-orphans (dependents dependency))
-(define-message $resolve-source (source requesting-directory order))
+(define-message $resolve-source (source requesting-directory))
 (define-message $on-bad-digest (info))
 (define-message $on-bad-signature (info))
 (define-message $on-missing-signature (info))
 (define-message $on-unverified-host (host))
 (define-message $on-package-installed (info))
 (define-message $on-package-uninstalled (info))
-(define-message $install-package (info path source))
-(define-message $download-package (info catalog-url source))
+(define-message $install-package (source info url-or-path remote?))
 (define-message $uninstall-package (dependency-string))
+
+
+
+(define (zcpkg-start-team!)
+  (define team (new zcpkg-team%))
+  (define started? #f)
+
+  (define (work tasks)
+    (unless started?
+      (send team start!
+            (λ () (new outside-place%
+                       [make-place (λ () (place pch (zcpkg-worker-main pch)))]
+                       [make-start-message (λ () ($start (workspace-directory)))]
+                       [make-stop-message (λ () ($stop))])))
+      (set! started? #t)))
+
+  (define (stop)
+    (send team stop!))
+
+  (values work stop))
+
 
 ; Coordinate async work dedicated to package management.
 (define zcpkg-team%
@@ -51,39 +75,19 @@
       (and (super idle?)
            (null? frontlog)))
 
-    ; A lot goes on here. This processes the given tasks,
-    ; prioritizing work discovered along the way.  ("Hey, I found
-    ; this dependency. We need to take care of it first", etc). The
-    ; return value is a task backlog that the user can review before
-    ; deciding how to proceed.
     (define/public (process-frontlog! tasks)
       (set! frontlog (append tasks frontlog))
       (loop)
-      (define-values (for-review seen)
-        (for*/fold ([for-review null]
-                    [seen (set)])
-                   ([schedule (in-list backlog)]
-                    [next (in-schedule schedule)])
-          (if (set-member? seen next)
-              (values for-review
-                      seen)
-              (values (cons next for-review)
-                      (set-add seen next)))))
-
-      (reverse for-review))
+      backlog)
 
     (define/public (handle-$done id finished-job)
       (define reporter (list-ref (get-field workers this) id))
       (if (null? frontlog)
           (send reporter value #f)
-          (let*-values ([(next-job remaining) (get-next-message (car frontlog))])
-            (reporter ($schedule-dependent next-job))
-            (set! frontlog
-                  (if remaining
-                      (cons remaining (cdr frontlog))
-                      (cdr frontlog))))))
+          (begin (send reporter value (car frontlog))
+                 (set! frontlog (cdr frontlog)))))
 
-    (define/public (handle-$crash id exn-string)
+    (define/public (handle-$fail id exn-string)
       (stop!)
       (raise (exn:fail:user
               (format "A worker crashed. Please report this to the developer:~n~a~n"
@@ -97,14 +101,34 @@
       (set! backlog (cons v backlog)))))
 
 
-
 (define zcpkg-worker%
   (class actor%
     (super-new)
+    (init-field pch)
+    (inherit recv)
+
+    (define/override (idle?)
+      #f)
+
+    (define/public (send-up v)
+      (place-channel-put pch v))
+
+    (define/public (send-to-backlog v)
+      (send-up ($backlog v)))
+
+    (define/public (send-to-frontlog v)
+      (send-up ($frontlog v)))
+
+    (define/override (loop)
+      (let-values ([(message-id args) (destructure-message (sync (recv)))])
+        (apply dynamic-send this (message-id->method-id message-id) args)
+        (send-up ($done))
+        (loop)))
 
     (define/public (handle-$start workspace-dir)
       (workspace-directory workspace-dir)
-      (load-zcpkg-settings!))
+      (load-zcpkg-settings!)
+      (send-up ($done)))
 
     (define/public (handle-$resolve-source)
       (void))
@@ -118,71 +142,39 @@
                   [(const #t)
                    (λ (e)
                      (place-channel-put pch
-                                        ($crash (if (exn? e) (exn->string e) (~s e)))))])
-    (send (new zcpkg-worker%) loop)))
-
-(define (zcpkg-start-team!)
-  (define team (new zcpkg-team%))
-  (define started? #f)
-
-  (define (send-work tasks)
-    (unless started?
-      (send team start!
-            (λ () (new outside-place%
-                       [make-place (λ () (place pch (zcpkg-worker-main pch)))]
-                       [make-start-message (λ () ($start (workspace-directory)))]
-                       [make-stop-message (λ () ($stop))])))
-      (set! started? #t))
-    (send team process-frontlog! tasks))
-
-  (define (stop)
-    (send team stop!))
-
-  (values send-work stop))
+                                        ($fail (if (exn? e) (exn->string e) (~s e)))))])
+    (send (new zcpkg-worker% [pch pch]) loop)))
 
 
-(define (get-next-message node)
-  (define deps ($schedule-dependencies node))
-  (define first-dep (if (null? deps) #f (car deps)))
-  (if first-dep
-      (let-values ([(next-job variant) (get-next-message first-dep)])
-        (values next-job
-                ($schedule ($schedule-dependent node)
-                           (if variant
-                               (cons variant (cdr deps))
-                               (cdr deps)))))
-      (values node #f)))
+(define (sort-dependent-work output)
+  (define-values ($edges $vertices+rest) (partition $edge? output))
+  (define-values ($vertices remaining) (partition $vertex? $vertices+rest))
 
+  (define-values (vertices edges)
+    (values (for/mutable-set ([v (in-list $vertices)])
+                             ($vertex-label v))
+            (for/mutable-set ([e (in-list $edges)])
+                             (cons ($edge-dependent e) ($edge-dependency e)))))
 
-(define (in-schedule s)
-  (in-generator
-   (let loop ([remaining s])
-     (and remaining
-          (let-values ([(next remaining*) (get-next-message remaining)])
-            (yield ($schedule-dependent next))
-            (loop remaining*))))))
+  ; Khan's topological sort follows.
+  (define leaves (set-copy vertices))
+  (for ([e (in-mutable-set edges)])
+    (set-remove! leaves (cdr e)))
 
+  (define sorted
+    (let loop ([wip null])
+      (unless (set-empty? leaves)
+        (define n (set-first leaves))
+        (set-remove! leaves n)
+        (for ([m (in-set vertices)] #:when (set-member? edges (cons n m)))
+          (set-remove! edges (cons n m))
+          (unless (for/or ([e (in-set edges)]) (equal? (cdr e) m))
+            (set-add! leaves m)))
+        (loop (cons n wip)))))
 
-(module+ test
-  (require rackunit
-           racket/sequence)
-
-  (test-case "Resolve dependencies first"
-    (define $ $schedule)
-
-    (define jobs
-      ($ 'root
-         (list
-          ($ 'a
-             (list ($ 'x null)
-                   ($ 'y null)
-                   ($ 'z
-                      (list ($ 'q null)))))
-          ($ 'b
-             (list ($ 'r null)
-                   ($ 's (list ($ 'q null))))))))
-
-
-    (check-equal?
-     (sequence->list (in-schedule jobs))
-     '(x y q z a r q s b root))))
+  ; The package manager allows dependency cycles, but will need to
+  ; know if they exist to warn the user.
+  (define acyclic? (set-empty? edges))
+  (values acyclic?
+          (if acyclic? sorted (map $vertex-label $vertices))
+          remaining))
