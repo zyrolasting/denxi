b9145b0916fe730481cadf1f885bc4282866a2f3
Sage Gerard <sage@sagegerard.com>
2020-07-19 12:02:17 -0400

    Adapt source->maybe-path to tests

--- source.rkt
+++ source.rkt
@@ -28,32 +28,35 @@
         [(dependency? v) (dependency->string v)]
         [(path? v) (path->string v)]))
 
-(define (source->maybe-path v [relative-path-root (current-directory)])
+(define (source->maybe-path #:must-exist? [must-exist? #t] v [relative-path-root (current-directory)])
   (cond [(path? v)
          (define pkg-path
            (simplify-path (if (complete-path? v) v
                               (build-path relative-path-root v))))
 
          (if (directory-exists? pkg-path) pkg-path
-             (raise (exn:fail:user
-                     (format (~a "Package not found: ~a~n~n"
-                                 "Assuming your command is correct, a package~n"
-                                 "may have an incorrect relative path as a dependency.~n~n"
-                                 "Base path: ~a~n"
-                                 "Relative path: ~a~n")
-                             pkg-path
-                             relative-path-root
-                             (find-relative-path relative-path-root pkg-path))
-                     (current-continuation-marks))))]
+             (and must-exist?
+                  (raise (exn:fail:user
+                          (format (~a "Package not found: ~a~n~n"
+                                      "Assuming your command is correct, a package~n"
+                                      "may have an incorrect relative path as a dependency.~n~n"
+                                      "Base path: ~a~n"
+                                      "Relative path: ~a~n")
+                                  pkg-path
+                                  relative-path-root
+                                  (find-relative-path relative-path-root pkg-path))
+                          (current-continuation-marks)))))]
 
         [(url? v)
          (and (or (not (url-scheme v))
                   (equal? (url-scheme v) "file"))
-              (source->maybe-path (url->maybe-path v relative-path-root)
+              (source->maybe-path #:must-exist? must-exist?
+                                  (url->maybe-path v relative-path-root)
                                   relative-path-root))]
 
         [(url-string? v)
-         (source->maybe-path (string->url v)
+         (source->maybe-path #:must-exist? must-exist?
+                             (string->url v)
                              relative-path-root)]
 
         [else #f]))
@@ -138,22 +141,22 @@
 
   (parameterize ([current-directory ./])
     (test-false "If it looks like a file path, then it doesn't count"
-                (source->maybe-path "./source.rkt"))
+                (source->maybe-path #:must-exist? #f "./source.rkt"))
 
     (test-equal? "If it looks like a directory path, then use it."
                  (source->maybe-path ".")
                  (simplify-path "."))
 
     (test-equal? "Mimic rules for file:// url pointing to a file."
-                 (source->maybe-path "file:///./source.rkt")
-                 (source->maybe-path "./source.rkt"))
+                 (source->maybe-path #:must-exist? #f "file:///./source.rkt")
+                 (source->maybe-path #:must-exist? #f "./source.rkt"))
 
     (test-equal? "Mimic rules for file:// url pointing to a directory."
-                 (source->maybe-path "file:///.")
-                 (source->maybe-path "."))
+                 (source->maybe-path #:must-exist? #f "file:///.")
+                 (source->maybe-path #:must-exist? #f "."))
 
     (test-false "Don't make paths out of things that don't exist."
-                (source->maybe-path "weeee.erk")))
+                (source->maybe-path #:must-exist? #f "weeee.erk")))
 
   (test-true "All valid dependency strings are valid sources"
              (andmap (λ (s) (dependency? (source->maybe-dependency s)))
