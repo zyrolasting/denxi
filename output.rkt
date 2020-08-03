d57a01cf8f032b1607503589f113b4733a0c22b5
Sage Gerard <sage@sagegerard.com>
2020-08-03 18:03:10 -0400

    Refactor output.rkt

--- output.rkt
+++ output.rkt
@@ -1,135 +1,99 @@
 #lang racket/base
 
-; Define a small text protocol for exchanging Racket hashes over ports.
+; Define program output
 
-(require racket/contract
-         racket/fasl
-         racket/generator
-         racket/hash
-         racket/match
+(require racket/fasl
          racket/pretty
-         racket/sequence
-         racket/serialize)
-
-(define output-format?
-  (or/c 'datum 'fasl 'report))
+         racket/serialize
+         "contract.rkt"
+         "format.rkt"
+         "message.rkt"
+         "zcpkg-settings.rkt"
+         "zcpkg-messages.rkt")
 
 (provide
- <<
  (contract-out
-  [current-hash-predicate (parameter/c (-> hash? any/c))]
-  [write-output (->* (hash?) (#:format output-format? output-port?) void?)]
-  [read-output (-> any/c hash?)]))
+  [write-output (->* ($message?) (output-port?) void?)]))
 
-(define current-output-format (make-parameter 'datum))
+(define important-messages
+  (list $already-installed?
+        $on-compilation-error?
+        $on-bad-digest?
+        $on-bad-signature?
+        $on-missing-signature?
+        $on-unverified-host?
+        $on-package-installed?
+        $install-package?))
 
-(define (write-output v #:format [fmt (current-output-format)] [out (current-output-port)])
-  (when ((current-hash-predicate) v)
+(define (include-output? m)
+  (or (ZCPKG_VERBOSE)
+      (ormap (λ (?) (? m)) important-messages)))
+
+(define (write-output v [out (current-output-port)])
+  (when (include-output? v)
     (parameterize ([current-output-port out])
-      (printf "~s " fmt)
-      ((case fmt
-         [(fasl) write-fasl]
-         [(datum) write-datum]
-         [(report) write-report])
-       v)
+      (define to-send
+        (if (ZCPKG_READER_FRIENDLY_OUTPUT)
+            v
+            (format-zcpkg-message v)))
+
+      (if (ZCPKG_FASL_OUTPUT)
+          (s-exp->fasl (serialize to-send) (current-output-port))
+          (if (ZCPKG_READER_FRIENDLY_OUTPUT)
+              (pretty-write #:newline? #t to-send)
+              (displayln to-send)))
       (flush-output))))
 
-
-(define (read-output in)
-  (define v (read in))
-  (if (eof-object? v) v
-      (case v
-        [(datum) (read in)]
-        [(fasl) (read-fasl in)]
-        [else (error 'read-output
-                     "Unsupported value in port: ~a"
-                     v)])))
-
-
-(define (in-output [in (current-input-port)])
-  (in-port read-output in))
-
-(define <<
-  (make-keyword-procedure
-   (λ (kws kwas fmt-message . args)
-     (write-output
-      (hash-set (make-immutable-hash
-                 (map (λ (kw a) (cons (string->symbol (keyword->string kw)) a))
-                      kws
-                      kwas))
-                'message
-                (apply format fmt-message args))))))
-
-
-(define (rewrite-output [in (current-input-port)])
-  (sequence-for-each write-output (in-output in)))
-
-(define (write-fasl v)
-  (fasl->s-exp (serialize v) (current-output-port)))
-
-(define (read-fasl in)
-  (deserialize (fasl->s-exp in)))
-
-(define (write-datum v)
-  (pretty-write #:newline? #t v))
-
-(define (write-report v)
-  (define message (hash-ref v 'message "not set"))
-  (define no-msg (hash-remove v 'message))
-
-  (define (write-entry k v)
-    (printf "~a := ~s~n" k v))
-
-  (write-entry 'message message)
-  (for ([(k v) (in-hash no-msg)])
-    (write-entry k v))
-  (display (newline)))
-
-(define (read-report in [h (hash)])
-  (define next (read-line in))
-  (if (equal? next "")
-      h
-      (read-report in
-                   (hash-set h
-                             (read in)
-                             (begin (read in) ; discard :=
-                                    (read in))))))
-
-
-(define pretty-output?
-  (make-parameter #f (λ (v) (and v #t))))
-
-(define current-hash-predicate
-  (make-parameter (λ _ #t)))
-
 (module+ test
-  (require rackunit)
+  (require rackunit
+           "setting.rkt"
+           "zcpkg-info.rkt"
+           (submod "zcpkg-info.rkt" test))
 
-  (define-values (i o) (make-pipe))
+  (define (capture-bytes p)
+    (define buffer (open-output-bytes))
+    (p buffer)
+    (get-output-bytes buffer #t))
 
-  (define h (hash 'message "yay"))
-  (write-output h o)
-  (test-equal? "Can reproduce"
-               (read-output i)
-               h))
+  (define (test-output msg v expected)
+    (define buffer (open-output-bytes))
+    (write-output v buffer)
+    (test-true msg
+               (if (or (regexp? expected)
+                       (pregexp? expected))
+                   (regexp-match? expected (get-output-bytes buffer #t))
+                   (equal? (get-output-bytes buffer #t) expected))))
 
+  (test-output "By default, program output is human-friendly"
+               ($already-installed dummy-zcpkg-info)
+               #px"already installed at")
 
+  (assume-settings ([ZCPKG_READER_FRIENDLY_OUTPUT #t])
+    (test-output "Allow reader-friendly output"
+                 ($already-installed dummy-zcpkg-info)
+                 (capture-bytes
+                  (λ (o)
+                    (pretty-write #:newline? #t ($already-installed dummy-zcpkg-info) o)))))
 
-(module+ main
-  (require racket/cmdline)
-  (command-line
-   #:once-any
-   [("--fasl")
-    "Use fast-load serialization for printed data"
-    (current-output-format 'fasl)]
+  (assume-settings ([ZCPKG_READER_FRIENDLY_OUTPUT #t]
+                    [ZCPKG_FASL_OUTPUT #t])
+    (test-case "Allow FASL output"
+      (define in
+        (open-input-bytes
+         (capture-bytes
+          (λ (o) (write-output ($already-installed dummy-zcpkg-info) o)))))
 
-   [("--write")
-    "Use write to print data"
-    (current-output-format 'datum)]
+      (check-equal? (deserialize (fasl->s-exp in))
+                    ($already-installed dummy-zcpkg-info))))
 
-   [("--report")
-    "Use write-report to print data"
-    (current-output-format 'report)]
+  (test-case "Control verbose output"
+    (assume-settings ([ZCPKG_VERBOSE #f])
+                     (test-output "Opt out of verbose output"
+                                  ($fail 1)
+                                  #""))
 
-   #:args ()
-   (rewrite-output)))
+    (assume-settings ([ZCPKG_VERBOSE #t]
+                      [ZCPKG_READER_FRIENDLY_OUTPUT #t])
+                     (test-output "Opt into verbose output"
+                                  ($fail 1)
+                                  #px"\\$fail"))))
