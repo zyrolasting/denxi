8734cc6ac5a930b7203bb1f407c0e8a58d3034a6
Sage Gerard <sage@sagegerard.com>
2020-08-17 01:49:05 -0400

    Remove manual output declarations

--- derivation.rkt
+++ derivation.rkt
@@ -8,8 +8,7 @@
 ; - Verify the content of a directory.
 ; - Recover from catestrophic failure (e.g. power outage)
 
-(provide (struct-out zcpkg-derivation-output)
-         (struct-out zcpkg-derivation)
+(provide (struct-out zcpkg-derivation)
          build-derivation!
          in-build-schedule
          in-derivations)
@@ -25,25 +24,15 @@
          net/base64
          "db.rkt"
          "file.rkt"
-         "store.rkt"
          "verify.rkt")
 
-(struct zcpkg-derivation-output (expected-digest path make!))
 
-(struct zcpkg-derivation (name inputs outputs)
+(struct zcpkg-derivation (name inputs process!)
   #:property prop:procedure
   (λ (self dir)
     (parameterize ([current-directory dir])
-      (for ([output (in-list (zcpkg-derivation-outputs self))])
-        (match-define (zcpkg-derivation-output expected-digest path make!) output)
-        (make! (zcpkg-derivation-inputs self))
-        (if expected-digest
-            (unless (equal? (make-digest path) expected-digest)
-              (error (format "~s failed to produce the exact ~s it promised."
-                             (zcpkg-derivation-name self)
-                             path)))
-            ; Use this to trigger an error if the output path does not exist.
-            (void (file-or-directory-identity path)))))))
+      (for ([output (in-directory)])
+        ((zcpkg-derivation-process! self))))))
 
 
 (define (in-broken-derivations)
@@ -51,6 +40,7 @@
                      (match-define (vector id name digest errors) row)
                      (not (sql-null? errors)))
                    (in-derivations)))
+
 
 (define (in-build-schedule drvs)
   (define (schedule-build drv)
@@ -60,31 +50,43 @@
    (for ([drv (in-list drvs)])
      (schedule-build drv))))
 
+
 (define (in-derivations)
   (in-query+ "select * from derivations;"))
 
-(define (delete-broken-derivations store)
-  (for* ([broken (in-broken-derivations)]
-         [path (zcpkg-derivation-output broken)])
+
+(define (make-derivation-directory-name digest name)
+  (format "~a-~a" digest name))
+
+
+(define (delete-broken-derivations basedir)
+  (for ([broken (in-broken-derivations)])
+    (match-define (vector _ name digest _) broken)
     (with-handlers ([exn:fail:filesystem? void])
-      (delete-directory/files path))))
+      (delete-directory/files
+       (build-path basedir (make-derivation-directory-name digest name))))))
 
 
 (define (build-derivation! basedir drv)
   (define name (zcpkg-derivation-name drv))
   (define digest (make-derivation-hash drv))
   (define hash-string (base64-encode digest #""))
-  (define dir (build-path basedir (format "~a-~a" digest name)))
+  (define dir (build-path basedir (make-derivation-directory-name digest name)))
 
-  (define errors
-    (with-handlers ([(const #t) (λ (e) (list (exn->string e)))])
-      (make-directory dir)
-      (drv dir)))
+  (if (valid-derivation-exists? digest dir)
+      dir
+      (let ([errors
+             (with-handlers ([(const #t) (λ (e) (list (exn->string e)))])
+               (make-directory dir)
+               (drv dir))])
+        (record-derivation-state! drv errors)
+        (and (null? errors)
+             dir))))
 
-  (record-derivation-state! drv errors)
-  (and (null? errors)
-       dir))
-
+(define (valid-derivation-exists? digest dir)
+  (and (directory-exists? dir)
+       (sql-null? (query-maybe-value+ "select errors from derivations where digest=?;"
+                                      digest))))
 
 (define (make-derivation-hash drv)
   (make-digest
@@ -94,11 +96,7 @@
                 (in-generator
                  (yield (string->bytes/utf-8 (zcpkg-derivation-name drv)))
                  (for ([input-drv (in-list (zcpkg-derivation-inputs drv))])
-                   (yield (make-derivation-hash drv)))
-                 (for ([output (in-list (zcpkg-derivation-outputs drv))])
-                   (match-define (zcpkg-derivation-output expected-digest path make!) output)
-                   (when expected-digest
-                     (yield expected-digest)))))))))
+                   (yield (make-derivation-hash drv)))))))))
 
 
 (define (record-derivation-state! drv digest errors)
@@ -108,6 +106,19 @@
                digest
                error-datum
                error-datum))
+
+
+(define (make-items-table)
+(query-exec+ #<<EOS
+CREATE TABLE IF NOT EXISTS items (
+       id INTEGER NOT NULL PRIMARY KEY,
+       derivation_id INTEGER NOT NULL,
+       path TEXT,
+       digest BLOB,
+       FOREIGN KEY derivation_id REFERENCES derivations(id)
+);
+EOS
+))
 
 
 (define (make-derivation-table)
