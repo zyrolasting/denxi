86c0b0454ef84aea63f7247361f27b4fbc1878fc
Sage Gerard <sage@sagegerard.com>
2020-08-17 01:49:30 -0400

    Allow list expression of byte strings

--- encode.rkt
+++ encode.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
-(require "contract.rkt")
+(require racket/function
+         racket/match
+         "contract.rkt")
 
 (define zcpkg-encodings
   '(base64 base32 b32 b64))
@@ -21,6 +23,19 @@
 (require net/base64
          base32)
 
+(define (interpret-byte-string-expression variant)
+  (match variant
+    [(list (? zcpkg-encoding/c encoding)
+           (? (disjoin string? bytes?) str))
+     (decode-bytes encoding str)]
+
+    [(? bytes? variant)
+     variant]
+
+    [_ (error 'interpret-byte-string-expression
+              "Not a zcpkg byte expression: ~s"
+              variant)]))
+
 (define (encode-bytes encoding bstr)
   (case encoding
     [(base32 b32) (base32-encode-bytes bstr)]
@@ -28,12 +43,22 @@
 
 (define (decode-bytes encoding encoded)
   (case encoding
-    [(base32 b32) (base32-decode-bytes encoded)]
-    [(base64 b64) (base64-decode encoded)]))
+    [(base32 b32)
+     (base32-decode-bytes
+      (if (string? encoded)
+          encoded
+          (bytes->string/utf-8 encoded)))]
+    [(base64 b64)
+     (base64-decode (if (string? encoded)
+                        (string->bytes/utf-8 encoded)
+                        encoded))]))
 
 (module+ test
   (require rackunit)
   (for ([encoding (in-list zcpkg-encodings)])
     (test-equal? (format "Encode and decode a message using ~a" encoding)
                  (decode-bytes encoding (encode-bytes encoding #"abc"))
+                 #"abc")
+    (test-equal? (format "Interpret ~a in byte expressions" encoding)
+                 (interpret-byte-string-expression `(,encoding ,(encode-bytes encoding #"abc")))
                  #"abc")))
