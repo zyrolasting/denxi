e7e84f7e635b7de5f090d099a42065516c0033df
Sage Gerard <sage@sagegerard.com>
2020-08-25 13:12:25 -0400

    Harden message.rkt and move relevant code to actor.rkt

--- actor.rkt
+++ actor.rkt
@@ -6,8 +6,19 @@
          (all-from-out racket/class))
 
 (require racket/class
+         racket/string
          "message.rkt")
 
+(define (destructure-message message)
+  (let ([message (vector->list (struct->vector message))])
+    (values (car message)
+            (cdr message))))
+
+(define (message-id->method-id message-id)
+  (string->symbol
+   (string-replace
+    (symbol->string message-id)
+    "struct:" "handle-")))
 
 (define actor%
   (class object%
@@ -22,17 +33,27 @@
            racket/set
            rackunit)
 
-  (define-message $m (v))
+  (define-message $derived (a b c))
 
-  (test-case "Call actor% methods with messages"
-    (define got-$m #f)
-    (define dumb-actor%
-      (class actor%
-        (define/public (handle-$m a)
-          (test-eq? "Got $m value" a 1)
-          (set! got-$m #t))
-        (super-new)))
+  (test-case "Destructure a $message"
+    (define msg ($derived 1 2 3))
+    (define-values (key args) (destructure-message msg))
+    (check-eq? key 'struct:$derived)
+    (check-equal? args '(1 2 3))
 
-    (define a (new dumb-actor%))
-    (send a pump ($m 1))
-    (test-true "Call $m handler" got-$m)))
+    (test-equal? "Convert symbol for message id to method id"
+                 (message-id->method-id key)
+                 'handle-$derived)
+
+    (test-case "Call actor% methods with messages"
+      (define got #f)
+      (define dumb-actor%
+        (class actor%
+          (define/public (handle-$derived . args)
+            (test-equal? "Reconstruct message in method" (apply $derived args) msg)
+            (set! got #t))
+          (super-new)))
+
+      (define a (new dumb-actor%))
+      (send a pump msg)
+      (test-true "Call handler from message" got))))
--- message.rkt
+++ message.rkt
@@ -1,36 +1,38 @@
 #lang racket/base
 
-; Define an async message type and any related operations.
+; Define the root of a prefab structure type tree.
+; Prefab structures are flexible enough to accomodate all
+; of the following use cases at once:
+;
+;  - Visibility into data when something breaks
+;  - Transmit data over place channels or ports
+;  - Return values for monadic operations
+;  - Compose data in custom shapes
 
-(provide (all-defined-out))
 
-(require racket/string
-         (for-syntax racket/base))
+(provide (struct-out $message)
+         define-message
+         define+provide-message)
 
 ; Make prefab for use on place channels.
 (struct $message () #:prefab)
 
-(define-syntax-rule (define-message id (fields ...))
-  (struct id $message (fields ...) #:prefab))
+(define-syntax define-message
+  (syntax-rules ()
+    [(_ id super-id (fields ...))
+     (struct id super-id (fields ...) #:prefab)]
+    [(_ id (fields ...))
+     (define+provide-message id $message (fields ...))]))
 
-(define (destructure-message message)
-  (let ([message (vector->list (struct->vector message))])
-    (values (car message)
-            (cdr message))))
-
-
-(define (message-id->method-id message-id)
-  (string->symbol
-   (string-replace
-    (symbol->string message-id)
-    "struct:" "handle-")))
+(define-syntax-rule (define+provide-message id rem ...)
+  (begin (provide id)
+         (define-message id rem ...)))
 
 (module+ test
   (require rackunit)
 
   (define-message $derived (a b c))
+  (define-message $ext $derived (d))
 
-  (test-case "Destructure a $message"
-    (define-values (key args) (destructure-message ($derived 1 2 3)))
-    (check-eq? key 'struct:$derived)
-    (check-equal? args '(1 2 3))))
+  (test-pred "Extend $message by default" $message? ($derived 1 2 3))
+  (test-pred "Extend other message if desired" $derived? ($ext 1 2 3 4)))
