13741f45b9e72da0e01628ec98f8bdcf198b0822
Sage Gerard <sage@sagegerard.com>
2020-10-07 10:14:48 -0400

    Remove url->maybe-path

--- url.rkt
+++ url.rkt
@@ -8,7 +8,6 @@
 (provide (all-from-out net/url)
          (contract-out
           [indicates-fs-path? (-> url? boolean?)]
-          [url->maybe-path (->* (url?) (path-string?) (or/c #f path?))]
           [url-string? predicate/c]))
 
 
@@ -16,19 +15,6 @@
   (with-handlers ([exn:fail? (λ _ #f)])
     (and (string->url s)
          #t)))
-
-(define (url->maybe-path u [relative-path-root (current-directory)])
-  (with-handlers ([exn? (const #f)])
-    (simplify-path
-     (apply build-path
-            (if (and (url-path-absolute? u)
-                     (not (member (get-leading-path-element u) '(same up))))
-                (car (filesystem-root-list))
-                relative-path-root)
-            (filter-map (λ (pp)
-                          (and (not (equal? "" (path/param-path pp)))
-                               (path/param-path pp)))
-                        (url-path u))))))
 
 (define (indicates-fs-path? u)
   (define leading (get-leading-path-element u))
