54b457f0758392aa45662af78481c708b37b51be
Sage Gerard <sage@sagegerard.com>
2020-10-07 10:17:18 -0400

    Remove dead code from url.rkt

--- url.rkt
+++ url.rkt
@@ -1,13 +1,10 @@
 #lang racket/base
 
-(require racket/function
-         racket/list
-         net/url
+(require net/url
          "contract.rkt")
 
 (provide (all-from-out net/url)
          (contract-out
-          [indicates-fs-path? (-> url? boolean?)]
           [url-string? predicate/c]))
 
 
@@ -16,23 +13,14 @@
     (and (string->url s)
          #t)))
 
-(define (indicates-fs-path? u)
-  (define leading (get-leading-path-element u))
-  (or (eq? 'up leading)
-      (eq? 'same leading)
-      (equal? "" leading) ; For UNC path (Windows)
-      (url-path-absolute? u)))
-
-
-(define (get-leading-path-element u)
-  (define path/params (url-path u))
-  (if (null? path/params)
-      (raise-argument-error 'get-leading-path-element "URL with a path" u)
-      (path/param-path (car path/params))))
-
 
 (module+ test
   (require rackunit)
 
-  (check-equal? (get-leading-path-element (string->url "https://example.com/blah;uh/foo?a=b#baz"))
-                "blah"))
+  (define valid-url-strings
+    '("foo"
+      "https://example.com"
+      "?a=b"))
+
+  (for ([valid valid-url-strings])
+    (check-pred url-string? valid)))
