ade7fffc4590d8fde6e2cc5e1e384d61950c7d8a
Sage Gerard <sage@sagegerard.com>
2020-12-26 14:04:56 -0500

    Remove unhelpful comment

--- main.rkt
+++ main.rkt
@@ -1,5 +1,4 @@
 #lang racket/base
-; Extend reader to use package definition module language.
 (require xiden/pkgdef)
 (provide (all-from-out xiden/pkgdef))
 (module reader syntax/module-reader xiden/pkgdef)
