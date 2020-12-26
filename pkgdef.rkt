0468d402bae0e624b2369c412b19a2de33eab3ca
Sage Gerard <sage@sagegerard.com>
2020-12-26 14:08:24 -0500

    Start building out pkgdef subcollection

--- pkgdef.rkt
+++ pkgdef.rkt
@@ -1,178 +1,31 @@
 #lang racket/base
 
-; Define a module language for derivations. Adapted from setup/infotab
-; https://raw.githubusercontent.com/racket/racket/master/racket/collects/setup/infotab.rkt
-
-(require racket/contract)
-
-(provide λ
-         #%app
-         #%datum
-         #%top
-         #%top-interaction
-         append
-         base32
-         base64
-         build-path
-         build-workspace-path
-         call-with-input
-         car
-         case
-         cdr
-         collection-path
-         cond
-         cons
-         current-directory
-         define
-         define-values
-         equal?
-         error
-         from-catalogs
-         from-file
-         getenv
-         hash
-         hash-clear
-         hash-ref
-         hash-remove
-         hash-set
-         hash-set*
-         hash-update
-         hex
-         if
-         input
-         input-ref
-         install
-         integrity
-         keep-input!
-         lambda
-         let
-         let-values
-         list
-         list*
-         make-immutable-hash
-         path->string
-         quasiquote
-         quote
-         reverse
-         run
-         signature
-         sources
-         string-append
-         system-library-subpath
-         unpack
-         unless
-         unquote
-         unquote-splicing
-         void
-         when
-         (rename-out [#%module-begin* #%module-begin]))
-
-
-(require (for-syntax racket/base)
-         racket/function
-         racket/list
-         racket/sequence
-         racket/string
-         racket/system
-         "archiving.rkt"
+(require (for-syntax "altdef.rkt" racket/base)
          "codec.rkt"
-         "compression.rkt"
-         "format.rkt"
+         "file.rkt"
          "input-info.rkt"
          "integrity.rkt"
-         "l10n.rkt"
-         "localstate.rkt"
-         "logged.rkt"
-         "message.rkt"
-         "monad.rkt"
-         "package.rkt"
-         "path.rkt"
-         "printer.rkt"
-         "rc.rkt"
-         "setting.rkt"
+         (rename-in "monad.rkt" [do do*])
          "signature.rkt"
-         "source.rkt"
-         "url.rkt"
-         "workspace.rkt")
+         "system.rkt")
 
-(define (install link-path output-name pkgdef)
-  (define result (run+print-log (run-package link-path output-name pkgdef)))
-  (if (eq? result FAILURE)
-      (raise-user-error 'install
-                        "Could not install package output ~a"
-                        output-name)
-      (void)))
-
-
-(define (run #:expected-exit-codes [expected-exit-codes '(0)]
-             #:timeout [timeout (XIDEN_SUBPROCESS_TIMEOUT_S)]
-             cmd . args)
-  (define-values (handle stdout stdin stderr)
-    (apply subprocess
-           (current-output-port)
-           #f
-           (current-output-port)
-           #f
-           (find-executable-path cmd)
-           args))
-
-  (or (sync/timeout (min timeout (XIDEN_SUBPROCESS_TIMEOUT_S)) handle)
-      (begin (subprocess-kill handle)
-             (raise-user-error 'run
-                               "~a timed out after ~a seconds. args: ~s"
-                               cmd
-                               timeout
-                               args)))
-
-  (list? (member (subprocess-status handle)
-                 expected-exit-codes)))
-
+(provide base64
+         base32
+         hex
+         input
+         integrity
+         signature
+         in-paths
+         run
+         <-
+         (except-out (all-from-out racket/base) do #%module-begin)
+         (rename-out [#%module-begin* #%module-begin]
+                     [list sources]
+                     [do* do]))
 
 (define-syntax (#%module-begin* stx)
   (syntax-case stx ()
-    [(mod-beg defn ...)
-     (let ([names (let loop ([defns (syntax->list #'(defn ...))]
-                             [r '()])
-                    (if (null? defns)
-                        (reverse r)
-                        (loop (cdr defns)
-                              (syntax-case (car defns) (define)
-                                [(define var val)
-                                 (identifier? #'var)
-                                 (cons #'var r)]
-                                [(define (var) val ...)
-                                 (identifier? #'var)
-                                 (cons #'var r)]
-                                [(define (var s ...) val ...)
-                                 (identifier? #'var)
-                                 (cons #'var r)]
-                                [(define-values (var) val)
-                                 (cons #'var r)]
-                                [_else (raise-syntax-error
-                                        'xiden
-                                        "not a well-formed package definition"
-                                        stx (car defns))]))))])
-       (let ([dup (check-duplicate-identifier names)])
-         (when dup
-           (raise-syntax-error 'xiden "duplicate definition" stx dup)))
-       (with-syntax ([(name ...) names]
-                     [#%info-domain (datum->syntax stx '#%info-domain)]
-                     [#%info-lookup (datum->syntax stx '#%info-lookup)])
-         #'(#%plain-module-begin
-            defn ...
-            (define #%info-lookup
-              (case-lambda
-                [(n) (#%info-lookup n (λ () (error 'xiden "~a not set" n)))]
-                [(n fail)
-                 (unless (and (procedure? fail)
-                              (procedure-arity-includes? fail 0))
-                   (error
-                    'xiden
-                    "expected second argument to be a procedure that takes no arguments, got: ~e"
-                    fail))
-                 (case n
-                   [(name) name]
-                   ...
-                   [else (fail)])]))
-            (define (#%info-domain) '(name ...))
-            (provide #%info-lookup #%info-domain))))]))
+    [(_ . body)
+     (with-syntax ([expanded (expand-pkgdef-module #'body stx)])
+       #'(#%module-begin expanded))]))
+
---
+++ pkgdef/expand.rkt
@@ -0,0 +1,170 @@
+#lang racket/base
+
+; Package definition expansion rules
+
+(provide expand-pkgdef-module
+         (struct-out pkgdef-state))
+
+(require (only-in version/utils
+                  valid-version?
+                  version<=?)
+         syntax/parse
+         syntax/strip-context
+         (only-in "racket-version.rkt"
+                  racket-version-selection)
+         (only-in "string.rkt"
+                  non-empty-string
+                  non-empty-string?
+                  string-join)
+         (only-in "url.rkt"
+                  url-string))
+
+;-------------------------------------------------------------------------------
+; Captures expansion state
+
+(struct pkgdef-state
+  (provider
+   package
+   edition
+   revision-number
+   revision-names
+   racket-versions
+   metadata
+   inputs
+   outputs
+   actions
+   tags
+   description
+   url
+   os-support)
+  #:transparent)
+
+(define ALL_OS_SYMS '(unix windows macosx))
+
+(define empty-pkgdef
+  (pkgdef-state "default"
+                "default"
+                "default"
+                0
+                null
+                null
+                null
+                null
+                null
+                null
+                null
+                ""
+                ""
+                ALL_OS_SYMS))
+
+
+(define-syntax-rule (set-field st field val)
+  (struct-copy pkgdef-state st [field val]))
+
+(define-syntax-class os-sym
+  (pattern (~var s id)
+           #:when
+           (member (syntax-e #'s)
+                   '(unix windows macosx))))
+
+
+(define (update state stx)
+  (syntax-parse stx
+    #:datum-literals
+    (action
+     description
+     edition
+     url
+     input
+     metadatum
+     output
+     os-support
+     package
+     provider
+     racket-versions
+     revision-names
+     revision-number
+     tags)
+
+    [(provider name:non-empty-string)
+     (set-field state provider (syntax-e #'name))]
+
+    [(package name:non-empty-string)
+     (set-field state package (syntax-e #'name))]
+
+    [(edition name:non-empty-string)
+     (set-field state edition (syntax-e #'name))]
+
+    [(revision-names name:non-empty-string ...)
+     (set-field state revision-names (syntax->datum #'(name ...)))]
+
+    [(racket-versions version:racket-version-selection ...+)
+     (set-field state racket-versions (syntax->datum #'(version ...)))]
+
+    [(revision-number num:exact-nonnegative-integer)
+     (set-field state revision-number (syntax-e #'num))]
+
+    [(metadatum name:id v:expr)
+     (set-field state metadata (cons (cons #'name #'v) (pkgdef-state-metadata state)))]
+
+    [(input name:non-empty-string _ ...)
+     (set-field state inputs (cons stx (pkgdef-state-inputs state)))]
+
+    [(output name:non-empty-string steps:expr ...+)
+     (set-field state outputs (cons (cons #'name #'(steps ...)) (pkgdef-state-outputs state)))]
+
+    [(os-support os:os-sym ...+)
+     (set-field state os-support (syntax->datum #'(os ...)))]
+    
+    [(tags tag:non-empty-string ...)
+     (set-field state tags (syntax->datum #'(tag ...)))]
+
+    [(description desc:non-empty-string ...+)
+     (set-field state description (string-join (syntax->datum #'(desc ...)) ""))]
+
+    [(url u:url-string)
+     (set-field state url (syntax-e #'u))]
+    
+    [(action (id:id sig:id ...+) body:expr ...+)
+     (set-field state actions (cons stx (pkgdef-state-actions state)))]))
+
+
+(define (expand-pkgdef-module-body state)
+  (with-syntax ([provider/patt (pkgdef-state-provider state)]
+                [package/patt (pkgdef-state-package state)]
+                [edition/patt (pkgdef-state-edition state)]
+                [url/patt (pkgdef-state-url state)]
+                [revision-number/patt (pkgdef-state-revision-number state)]
+                [(revision-names/patt ...) (pkgdef-state-revision-names state)]
+                [(os ...) (pkgdef-state-os-support state)]
+                [(tag ...) (pkgdef-state-tags state)]
+                [([metadata-key . metadata-val] ...) (pkgdef-state-metadata state)]
+                [([output-name . output-steps] ...) (pkgdef-state-outputs state)]
+                [([_ (action-sig ...) action-steps ...] ...) (pkgdef-state-actions state)]
+                [([_ (action-id _ ...) _ ...] ...) (pkgdef-state-actions state)]
+                [(input-expr ...) (pkgdef-state-inputs state)])
+    #'(begin (provide (except-out (all-defined-out) action-id ...))
+             (define provider provider/patt)
+             (define package package/patt)
+             (define edition edition/patt)
+             (define revision-number revision-number/patt)
+             (define revision-names '(revision-names/patt ...))
+             (define inputs (list input-expr ...))
+             (define url url/patt)
+             (define tags '(tag ...))
+             (define os-support '(os ...))
+             (define outputs-names '(output-name ...))
+             (begin (define (action-sig ...) (do action-steps ...)) ...)
+             (define (build target)
+               (case target
+                 [(output-name) (do . output-steps)]
+                 ...
+                 [else #f]))
+             (define metadata (make-immutable-hasheq '((metadata-key . metadata-val) ...))))))
+
+(define (expand-pkgdef-module data stx [state empty-pkgdef])
+  (syntax-case data ()
+    [(head . ())
+     (replace-context stx (expand-pkgdef-module-body (update state #'head)))]
+    [(head . tail)
+     (expand-pkgdef-module #'tail stx (update state #'head))]))
