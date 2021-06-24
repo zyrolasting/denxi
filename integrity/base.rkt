55edb6349c9c61f3738a0dbfa7f54a906843f55e
Sage Gerard <sage@sagegerard.com>
2021-06-24 16:37:59 -0400

    Simplify integrity/base.rkt

--- integrity/base.rkt
+++ integrity/base.rkt
@@ -9,7 +9,7 @@
          (contract-out
           [check-integrity
            (-> #:trust-bad-digest any/c
-               #:trust-message-digest-algorithms (listof symbol?)
+               (-> symbol? any/c)
                any/c
                bytes?
                $integrity?)]
@@ -25,111 +25,43 @@
 (define well-formed-integrity-info/c
   (struct/c integrity-info symbol? bytes?))
 
-(define (make-$integrity ok? p intinfo)
-  ($integrity ok? (object-name p) intinfo))
-
 
 (define (check-integrity #:trust-bad-digest trust-bad-digest
-                         #:trust-message-digest-algorithms trust-message-digest-algorithms
+                         trust-chf?
                          intinfo
-                         variant)
-  (consider-digest-trust #:trust-bad-digest trust-bad-digest intinfo
-   (λ () (consider-integrity-info #:trust-unknown-digest trust-bad-digest intinfo
-     (λ () (consider-chf-trust #:trust-message-digest-algorithms trust-message-digest-algorithms intinfo
-       (λ () (consider-digest-match intinfo variant))))))))
-
-
-
-; Danger! Trusting a bad digest means trusting arbitrary data
-(define (consider-digest-trust #:trust-bad-digest trust-bad-digest intinfo k)
+                         other-digest)
   (if trust-bad-digest
-      (make-$integrity #t consider-digest-trust intinfo)
-      (k)))
-
-
-; CHFs may have collisions, and we cannot predict InfoSec incidents.
-; Proceed only if the caller says they trust the CHF.
-(define (consider-chf-trust #:trust-message-digest-algorithms trusted intinfo k)
-  (if (member (integrity-info-algorithm intinfo) trusted)
-      (k)
-      (make-$integrity #f consider-chf-trust intinfo)))
-
-
-; We cannot reasonably compare digests if the integrity information is
-; malformed. Make sure we can use it.
-(define (consider-integrity-info #:trust-unknown-digest trust-unknown-digest intinfo k)
-  (if (well-formed-integrity-info/c intinfo)
-      (k)
-      (make-$integrity trust-unknown-digest
-                       consider-integrity-info
-                       intinfo)))
-
-
-; Compare the digests as the last step to protect preconditions.
-(define (consider-digest-match intinfo variant)
-  (call/cc
-   (λ (abort)
-     (make-$integrity
-      (equal? (integrity-info-digest intinfo) variant)
-      consider-digest-match
-      intinfo))))
+      ($integrity #t 'consider-digest-trust intinfo)
+      (if (well-formed-integrity-info/c intinfo)
+          (if (trust-chf? (integrity-info-algorithm intinfo))
+              ($integrity (equal? (integrity-info-digest intinfo) other-digest)
+                          'consider-digest-match
+                          intinfo)
+              ($integrity #f 'consider-chf-trust intinfo))
+          ($integrity trust-bad-digest 'consider-integrity-info intinfo))))
 
 
 (module+ test
-  (require racket/file
-           racket/function
-           rackunit)
-  
-  ; Coverage info should flag this as uncovered when tests are passing.
-  (define (fails . _)
-    (fail "Control should not have reached here"))
+  (require rackunit)
 
-  (define info
-    (integrity-info 'sha1 #""))
-  
-  (define (check-integrity-passed tbd tmda decider)
-    (check-equal? (check-integrity #:trust-bad-digest tbd
-                                   #:trust-message-digest-algorithms tmda
-                                   info
-                                   #"")
-                  ($integrity #t (object-name decider) info)))
-  
+  (define info (integrity-info 'whatever #""))
 
-  (test-case "Trust only certain CHFs"
-    (check-true (consider-chf-trust #:trust-message-digest-algorithms '(sha1)
-                                    info
-                                    (λ () #t)))
-    (check-integrity-passed #f '(sha1) consider-digest-match))
+  (test-equal? "Trust bad digests"
+               (check-integrity #:trust-bad-digest #t #f info #f)
+               ($integrity #t 'consider-digest-trust info))
 
-  
-  (test-equal? "Distrust uncited CHFs"
-               (consider-chf-trust #:trust-message-digest-algorithms '(md5)
-                                   info
-                                   fails)
-               ($integrity #f (object-name consider-chf-trust) info))
+  (test-equal? "Distrust CHFs"
+               (check-integrity #:trust-bad-digest #f (λ _ #f) info #f)
+               ($integrity #f 'consider-chf-trust info))
 
+  (test-equal? "Reject bad integrity info"
+               (check-integrity #:trust-bad-digest #f (λ _ #t) 'junk #f)
+               ($integrity #f 'consider-integrity-info 'junk))
 
-  (test-case "Skip integrity checking"
-    (check-equal? (consider-digest-trust #:trust-bad-digest #t info fails)
-                  ($integrity #t (object-name consider-digest-trust) info))
-    (check-integrity-passed #t null consider-digest-trust))
+  (test-equal? "Trust CHFs"
+               (check-integrity #:trust-bad-digest #f (λ _ #t) info #"wont match")
+               ($integrity #f 'consider-digest-match info))
 
-  
-  (test-true "Proceed with integrity checking"
-             (consider-digest-trust #:trust-bad-digest #f info (λ () #t)))
-  
-  (test-true (format "Count ~s as valid integrity info" info)
-             (consider-integrity-info #:trust-unknown-digest #f info (const #t)))
-  
-  (define (test-unknown v)
-    (define (check trust?)
-      (equal? (consider-integrity-info #:trust-unknown-digest trust? v fails)
-              ($integrity trust? (object-name consider-integrity-info) v)))
-
-    (test-true (format "Count ~s as undeclared integrity info" v)
-               (and (check #t)
-                    (check #f))))
-
-  (test-unknown 'garbage)
-  (test-unknown (integrity-info "" #"a"))
-  (test-unknown (integrity-info 'garbage "a")))
+  (test-equal? "Match digests"
+               (check-integrity #:trust-bad-digest #f (λ _ #t) info #"")
+               ($integrity #t 'consider-digest-match info)))
