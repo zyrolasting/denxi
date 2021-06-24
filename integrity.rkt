21adc04b74e1acd13194998f9f163c8bee9fa1e3
Sage Gerard <sage@sagegerard.com>
2021-06-24 12:36:37 -0400

    Draft refactor of integrity system
    - Split integrity.rkt into multiple files. This is necessary to avoid
      circular dependencies in `source.rkt` and `state.rkt`, which need
      lower-level functionality.
    - Replace `XIDEN_TRUST_CHFS` with `current-chfs` as source of trust.
      `XIDEN_TRUST_CHFS` will feed into `current-chfs`.
    - Allow configuration for CHF implementations, so that users can
      respond to security incidents in more nuanced ways.
    - Allow user-defined name resolution for CHFs (e.g. SHA-1 is an alias
      of sha1, but SHA-224 refers to the SHA-3 variant)

--- integrity.rkt
+++ integrity.rkt
@@ -1,90 +1,79 @@
 #lang racket/base
 
-; Verify integrity of bytes
+; User-facing API for integrity subsystem
 
-(require racket/contract
+(require racket/contract)
+(provide
+ (struct-out integrity-info/sourced)
+ (all-from-out "integrity/base.rkt"
+               "integrity/chf.rkt")
+ (contract-out
+  [XIDEN_TRUST_BAD_DIGEST setting?]
+  [XIDEN_TRUST_CHFS setting?]
+  [integrity
+   (-> symbol?
+       source-variant?
+       integrity-info/sourced?)]
+  [make-sourced-digest
+   (->* (source-variant? symbol?) (exhaust/c) bytes?)]
+  [fetch-digest
+   (-> well-formed-integrity-info/c exhaust/c any/c)]
+  [make-trusted-integrity-info
+   (->* (source-variant?)
+        (symbol?)
+        integrity-info?)]
+  [lock-integrity-info
+   (->* ((or/c integrity-info/sourced? well-formed-integrity-info/c))
+        (#:digest-budget budget/c exhaust/c)
+        well-formed-integrity-info/c)]
+  [load-builtin-chf
+   (-> symbol? (-> any) any)]
+  [bind-trust-list
+   (-> (listof (or/c integrity-info/sourced? integrity-info?))
+       (-> (or/c bytes? path-string? input-port?)
+           boolean?))]))
+
+(require (only-in racket/file file->bytes)
+         (only-in racket/list index-of)
          "crypto.rkt"
-         "file.rkt"
-         "format.rkt"
-         "state.rkt"
-         "message.rkt"
+         "integrity/base.rkt"
+         "integrity/chf.rkt"
+         "integrity/fallback.rkt"
+         "integrity/ffi.rkt"
          "port.rkt"
          "setting.rkt"
-         "source.rkt")
-
-(provide (struct-out integrity-info)
-         (contract-out
-          [MAX_EXPECTED_DIGEST_LENGTH
-           budget/c]
-          [integrity
-           (-> chf/c
-               source-variant?
-               integrity-info?)]
-          [make-sourced-digest
-           (->* (source-variant?
-                 chf/c)
-                (exhaust/c)
-                bytes?)]
-          [fetch-digest
-           (-> well-formed-integrity-info/c exhaust/c any/c)]
-          [well-formed-integrity-info/c
-           flat-contract?]
-          [bind-trust-list
-           (-> (listof well-formed-integrity-info/c)
-               (-> (or/c bytes? path-string? input-port?)
-                   boolean?))]
-          [make-trusted-integrity-info
-           (->* (source-variant?)
-                (chf/c)
-                integrity-info?)]
-          [lock-integrity-info
-           (->* (well-formed-integrity-info/c)
-                (#:digest-budget budget/c exhaust/c)
-                well-formed-integrity-info/c)]
-          [check-integrity
-           (-> #:trust-bad-digest any/c
-               #:trust-message-digest-algorithms (listof chf/c)
-               any/c
-               source-variant?
-               $integrity?)]))
-
-(define+provide-message $integrity (ok? stage info))
-(define+provide-setting XIDEN_TRUST_BAD_DIGEST boolean? #f)
-(define+provide-setting XIDEN_TRUST_CHFS (listof chf/c) null)
-
-(struct integrity-info (algorithm digest) #:prefab)
-
-(define integrity integrity-info)
-
-(define MAX_EXPECTED_DIGEST_LENGTH 128)
+         "source.rkt"
+         "state.rkt")
 
 
-(define (make-trusted-integrity-info source [chf DEFAULT_CHF])
-  (fetch (coerce-source source)
-         (λ (in est-size)
-           (integrity-info chf (make-digest in chf)))
-         raise))
-
-(define (digest-length-ok? info)
-  (equal? (bytes-length (integrity-info-digest info))
-          (bytes-length (make-digest #"whatever"
-                                     (integrity-info-algorithm info)))))
-
-(define well-formed-integrity-info/c
-  (struct/c integrity-info
-            chf/c
-            source-variant?))
+; High-level integrity claim that may contain a source.
+(struct integrity-info/sourced (chf digest-source))
+(define integrity integrity-info/sourced)
 
 
-(define (bind-trust-list trusted)
-  (λ (in)
-    (for/or ([integrity trusted])
-      ($integrity-ok? (check-integrity
-                       #:trust-bad-digest #f
-                       #:trust-message-digest-algorithms (XIDEN_TRUST_CHFS)
-                       integrity
-                       (make-digest in
-                                    (integrity-info-algorithm integrity)))))))
+(define-setting XIDEN_TRUST_BAD_DIGEST boolean? #f)
+(define-setting XIDEN_TRUST_CHFS (listof symbol?) null)
+
+
+(define (load-builtin-chf xiden-sym [fail-thunk (λ () (raise ($chf-unavailable xiden-sym)))])
+  (or (and (integrity-ffi-available?!)
+           (integrity-ffi-chf-available?! xiden-sym)
+           (λ (in _) (integrity-ffi-make-digest! in xiden-sym)))
+      (and (fallback-chf-available? xiden-sym)
+           (λ (in _) (fallback-make-digest in xiden-sym)))
+      (fail-thunk)))
+
+
+(define (call-with-simplified-chf-trust #:trust trust-chfs #:wrt wrt f)
+  (parameterize ([current-chfs
+                  (foldl
+                   (λ (trusted-chf wip)
+                     (cons (cons (list trusted-chf)
+                                 (load-builtin-chf trusted-chf))
+                           wip))
+                   wrt
+                   trust-chfs)])
+    (f)))
 
 
 (define (fetch-digest intinfo exhaust)
@@ -114,73 +103,50 @@
              exhaust)))
 
 
+(define (make-trusted-integrity-info source [chf (get-default-chf)])
+  (fetch (coerce-source source)
+         (λ (in est-size) (integrity-info chf (make-digest in chf)))
+         raise))
+
+
 (define (lock-integrity-info intinfo
                              #:digest-budget [digest-budget MAX_EXPECTED_DIGEST_LENGTH]
                              [exhaust raise])
-  (integrity-info
-   (integrity-info-algorithm intinfo)
-   (lock-source (integrity-info-digest intinfo)
-                digest-budget
-                exhaust)))
+  (if (integrity-info? intinfo)
+      intinfo
+      (integrity-info
+       (find-chf-canonical-name (integrity-info/sourced-chf intinfo)
+                                (current-chfs))
+       (lock-source (integrity-info/sourced-digest-source intinfo)
+                    digest-budget
+                    exhaust))))
 
 
-;; -------------------------------------------------------------------------------
-;; Affirmations
 
-(define (consider-digest-trust #:trust-bad-digest trust-bad-digest intinfo k)
-  (if trust-bad-digest
-      (make-$integrity #t consider-digest-trust intinfo)
-      (k)))
-
-(define (consider-chf-trust #:trust-message-digest-algorithms trust-message-digest-algorithms intinfo k)
-  (if (member (integrity-info-algorithm intinfo) trust-message-digest-algorithms)
-      (k)
-      (make-$integrity #f consider-chf-trust intinfo)))
-
-(define (consider-integrity-info #:trust-unknown-digest trust-unknown-digest intinfo k)
-  (if (well-formed-integrity-info/c intinfo)
-      (k)
-      (make-$integrity trust-unknown-digest
-                       consider-integrity-info
-                       intinfo)))
-
-
-(define (consider-digest-match intinfo variant)
-  (call/cc
-   (λ (abort)
-     (define (return v)
-       (make-$integrity v consider-digest-match intinfo))
-     (return (equal? (fetch-digest intinfo (λ _ (return #f)))
-                     (make-sourced-digest variant (integrity-info-algorithm intinfo)))))))
-
-
-(define (check-integrity #:trust-bad-digest trust-bad-digest #:trust-message-digest-algorithms trust-message-digest-algorithms intinfo variant)
-  (consider-digest-trust #:trust-bad-digest trust-bad-digest intinfo
-   (λ () (consider-integrity-info #:trust-unknown-digest trust-bad-digest intinfo
-     (λ () (consider-chf-trust #:trust-message-digest-algorithms trust-message-digest-algorithms intinfo
-       (λ () (consider-digest-match intinfo variant))))))))
-
-
-(define (make-$integrity ok? p intinfo)
-  ($integrity ok? (object-name p) intinfo))
-
+(define (bind-trust-list claims)
+  (let ([trusted (map lock-integrity-info claims)])
+    (λ (in)
+      (for/or ([integrity trusted])
+        ($integrity-ok? (check-integrity
+                         #:trust-bad-digest #f
+                         #:trust-message-digest-algorithms (XIDEN_TRUST_CHFS)
+                         integrity
+                         (make-digest in
+                                      (integrity-info-algorithm integrity))))))))
 
 (module+ test
-  (require racket/function
-           racket/runtime-path
-           rackunit)
-
-  ; Coverage info should flag this as uncovered when tests are passing.
-  (define (fails . _)
-    (fail "Control should not have reached here"))
+  (require racket/file
+           racket/function
+           rackunit
+           (submod "integrity/chf.rkt" test)
+           "codec.rkt"
+           "crypto.rkt"
+           "source.rkt")
 
   (define (make-dummy-integrity-info algorithm)
     (integrity-info algorithm
                     (make-digest (string->bytes/utf-8 (symbol->string algorithm))
                                  algorithm)))
-
-  (define-runtime-path integrity.rkt "integrity.rkt")
-  (define-runtime-path main.rkt "main.rkt")
 
   (test-case "Make trusted integrity info"
     (define val (make-trusted-integrity-info #"abc" 'md5))
@@ -206,54 +172,7 @@
   (test-case "Bind trust in data"
     (define trust?
       (bind-trust-list
-       (list (integrity-info 'sha1 (make-digest integrity.rkt 'sha1)))))
+       (list (integrity-info 'sha1 (make-digest test-digest-data 'sha1)))))
     (XIDEN_TRUST_CHFS '(sha1)
       (λ ()
-        (check-true (call-with-input-file* integrity.rkt trust?))
-        (check-false (call-with-input-file* main.rkt trust?)))))
-
-  (test-true "Trust only certain CHFs"
-             (consider-chf-trust #:trust-message-digest-algorithms '(md5)
-                                 (integrity-info 'md5 #"")
-                                 (λ () #t)))
-
-
-  (let ([info (integrity-info 'sha1 #"")])
-    (test-equal? "Distrust uncited CHFs"
-                 (consider-chf-trust #:trust-message-digest-algorithms '(md5)
-                                     info
-                                     fails)
-                 ($integrity #f
-                             (object-name consider-chf-trust)
-                             info)))
-
-
-  (test-case "Create integrity information"
-    (for ([algorithm (in-list cryptographic-hash-functions)])
-      (define info (make-dummy-integrity-info algorithm))
-      (check-pred integrity-info? info)
-      (check-eq? (integrity-info-algorithm info) algorithm)
-      (check-pred digest-length-ok? info)
-
-      (test-equal? "Skip integrity checking"
-                   (consider-digest-trust #:trust-bad-digest #t info fails)
-                   ($integrity #t (object-name consider-digest-trust) info))
-
-      (test-true "Proceed with integrity checking"
-                 (consider-digest-trust #:trust-bad-digest #f info (λ () #t)))
-
-
-      (test-case "Flag ill-formed integrity info as an unknown digest"
-        (define (test-unknown v)
-          (define (check trust?)
-            (equal? (consider-integrity-info #:trust-unknown-digest trust? v fails)
-                    ($integrity trust? (object-name consider-integrity-info) v)))
-          (test-true (format "Count ~s as undeclared integrity info" v)
-                     (and (check #t) (check #f))))
-
-        (test-true (format "Count ~s as valid integrity info" info)
-                   (consider-integrity-info #:trust-unknown-digest #f info (const #t)))
-
-        (test-unknown 'garbage)
-        (test-unknown (integrity-info 'garbage #"abc"))
-        (test-unknown (integrity-info 'garbage "abc"))))))
+        (check-true (trust? (open-input-bytes test-digest-data)))))))
