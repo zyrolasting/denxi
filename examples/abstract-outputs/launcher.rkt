c95602756eaeb101df492521de3ea10ce9153ed9
Sage Gerard <sage@sagegerard.com>
2021-07-02 11:26:27 -0400

    Edit spacing for readability

--- examples/abstract-outputs/launcher.rkt
+++ examples/abstract-outputs/launcher.rkt
@@ -1,8 +1,11 @@
 #lang xiden/launcher
+
 (current-chfs (list snake-oil-chf))
 (XIDEN_TRUST_BAD_DIGEST #t)
 (XIDEN_WORKSPACE (build-path (current-directory) "workspace"))
-(module+ main (launch-xiden!))
+
+(module+ main
+  (launch-xiden!))
 
 (module+ test
   (require (submod xiden/cli test))
--- examples/determinism/launcher.rkt
+++ examples/determinism/launcher.rkt
@@ -1,6 +1,9 @@
 #lang xiden/launcher
+
 (current-chfs (list snake-oil-chf))
 (XIDEN_FETCH_TOTAL_SIZE_MB +inf.0)
 (XIDEN_TRUST_BAD_DIGEST #t)
 (XIDEN_WORKSPACE (build-path (current-directory) "workspace"))
-(module+ main (launch-xiden!))
+
+(module+ main
+  (launch-xiden!))
