ae730bd16330bc24aba2b5338433121d4e1b66dd
Sage Gerard <sage@sagegerard.com>
2021-07-03 15:26:33 -0400

    Fix package-dependencies launcher

--- examples/package-dependencies/launch.rkt
+++ examples/package-dependencies/launch.rkt
@@ -1,6 +1,7 @@
 #lang xiden/launcher
 
-(XIDEN_WORKSPACE (build-path (current-directory) "workspace"))
+(current-chfs (list snake-oil-chf))
 (XIDEN_TRUST_BAD_DIGEST #t)
+(XIDEN_WORKSPACE (build-path (current-directory) "workspace"))
 
 (module+ main (launch-xiden!))
