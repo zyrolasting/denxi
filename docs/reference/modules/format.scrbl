054cbc7958233d38608a48731cdd1fd71a96ebb8
Sage Gerard <sage@sagegerard.com>
2022-02-02 00:09:01 -0500

    Trim denxi/format

--- docs/reference/modules/format.scrbl
+++ docs/reference/modules/format.scrbl
@@ -4,8 +4,7 @@
                     racket/contract
                     racket/match
                     racket/string
-                    denxi/format
-                    denxi/message]
+                    denxi/format]
                     "../../shared.rkt"]
 
 
@@ -79,67 +78,4 @@ platform-specific suffix.
 
 If @racket[trailing?] is @racket[#t], then the last line will also end
 in the suffix.
-}
-
-@defproc[(join-lines* [#:trailing? trailing? any/c #f]
-                      [#:suffix suffix (or/c char? string? #f)]
-                      [line string?] ...)
-                      string?]{
-Like @racket[join-lines], except lines are accumulated from formal
-arguments.
-}
-
-
-@section{Message Formatting}
-
-@tech{Messages} are useful for exchanging information without
-formatting concerns, but program output must include human-readable
-strings. This section therefore defines a @deftech{message formatter}
-as a procedure that translates a @tech{message} to a string. The
-bindings documented in this section pertain exclusively to message
-formatters.
-
-@defthing[message-formatter/c chaperone-contract? #:value (-> $message? string?)]{
-A contract that matches a @tech{message formatter}.
-}
-
-@defform[(message-formatter patts ...)]{
-Expands to @racket[(λ (m) (match m patts ...))].
-
-@racketblock[
-(define-message $foo (a b c))
-
-(code:comment "\"1 2 3\"")
-((message-formatter [($foo x y z) (format "~a ~a ~a" x y z)]) ($foo 1 2 3))
-]
-}
-
-@defform[(define-message-formatter id patts ...)]{
-Expands to @racket[(define id (message-formatter patts ...))]
-}
-
-@defform[(define+provide-message-formatter id patts ...)]{
-Expands to
-
-@racketblock[
-(begin (provide (contract-out [id message-formatter/c]))
-       (define-message-formatter id patts ...))]
-}
-
-@defproc[(combine-message-formatters [formatter message-formatter/c] ...) message-formatter/c]{
-Returns a @tech{message formatter} that uses each @racket[formatter]
-in the order passed.
-}
-
-@defthing[default-message-formatter message-formatter/c]{
-A @tech{message formatter} useful only for producing locale-independent fallback strings.
-}
-
-@defthing[current-message-formatter (parameter/c message-formatter/c) #:value default-message-formatter]{
-A @tech/reference{parameter} holding the @tech{message formatter} for
-use with @racket[format-message].
-}
-
-@defproc[(format-message [m $message?]) string?]{
-Equivalent to @racket[((current-message-formatter) m)].
 }
--- format.rkt
+++ format.rkt
@@ -4,50 +4,30 @@
 
 (require racket/contract
          racket/format
-         racket/match
          racket/pretty
          racket/string
          racket/struct
-         "stream.rkt"
-         "message.rkt")
+         "stream.rkt")
 
-(define message-formatter/c (-> $message? string?))
-
-(provide (all-from-out racket/contract ; For define+provide expansion
-                       racket/format)
-         define-message-formatter
-         define+provide-message-formatter
-         message-formatter
+(provide (all-from-out racket/format)
          (contract-out
           [format-value
            (-> any/c string?)]
           [indent-lines
            (-> (listof string?) (listof string?))]
           [join-lines
-           (->* ((listof string?)) (#:trailing? any/c #:suffix (or/c char? string? #f)) string?)]
-          [join-lines*
-           (->* () (#:trailing? any/c #:suffix (or/c char? string? #f)) #:rest (listof string?) string?)]
-          [message-formatter/c
-           contract?]
-          [current-message-formatter
-           (parameter/c message-formatter/c)]
-          [format-message
-           (-> $message? string?)]
-          [combine-message-formatters
-           (->* () #:rest (listof message-formatter/c) message-formatter/c)]))
+           (->* ((listof string?)) (#:trailing? any/c #:suffix (or/c char? string? #f)) string?)]))
 
-
-
-;-----------------------------------------------------------------------------
-; Conventional formatting procedures
 
 (define system-eol
   (if (equal? (system-type 'os) 'windows)
       "\r\n"
       "\n"))
 
+
 (define (indent-lines lines)
   (map (λ (s) (~a "  " s)) lines))
+
 
 (define (join-lines #:suffix [preferred-suffix #f] #:trailing? [trailing? #f] lines)
   (define suffix (select-eol preferred-suffix))
@@ -55,9 +35,6 @@
   (if trailing?
       (string-append joined suffix)
       joined))
-
-(define (join-lines* #:suffix [preferred-suffix #f] #:trailing? [trailing? #f] . lines)
-  (join-lines #:suffix preferred-suffix #:trailing? trailing? lines))
 
 
 (define (select-eol preferred-suffix)
@@ -126,65 +103,9 @@
                                               (map format-value vs)))))
 
 
-;-----------------------------------------------------------------------------
-; $message formatters
-
-(define (combine-message-formatters . formatters)
-  (λ (m) (format-message-aux m formatters)))
-
-(define (format-message-aux m formatters)
-  (if (null? formatters)
-      (error 'format-message "Unknown message type: ~s" m)
-      (with-handlers ([exn:misc:match? (λ (e) (format-message-aux m (cdr formatters)))])
-        ((car formatters) m))))
-
-(define-syntax-rule (message-formatter patts ...)
-  (λ (m) (match m patts ...)))
-
-(define-syntax-rule (define-message-formatter id patts ...)
-  (define id (message-formatter patts ...)))
-
-(define-syntax-rule (define+provide-message-formatter id patts ...)
-  (begin (provide (contract-out [id message-formatter/c]))
-         (define-message-formatter id patts ...)))
-
-(define+provide-message-formatter default-message-formatter
-  [($show-string v) v]
-  [($show-datum v) (pretty-format #:mode 'write v)]
-  [v (format-value v)])
-
-(define current-message-formatter
-  (make-parameter default-message-formatter))
-
-(define (format-message m)
-  ((current-message-formatter) m))
-
 (module+ test
   (require racket/format
-           rackunit
-           "setting.rkt")
+           rackunit)
 
   (check-equal? (indent-lines '("a" "b")) '("  a" "  b"))
-  (check-equal? (join-lines #:suffix "\n" '("a" "b" "c")) "a\nb\nc")
-  (check-equal? (join-lines* #:suffix #\| #:trailing? #t "a" "b" "c") "a|b|c|")
-
-  (define dummy ($show-string "Testing: Blah"))
-
-  (test-case "Use fallback strings for message formatting"
-    (check-equal? (format-message dummy)
-                  ($show-string-message dummy))
-    (check-equal? (format-message ($show-datum '(1 (2) 3)))
-                  (pretty-format #:mode 'write '(1 (2) 3)))
-    (check-equal? (format-message '(1 (2) 3))
-                  (~s '(1 (2) 3))))
-
-  (test-case "Compose message formatters"
-    (define-message $other (message))
-    (define-message-formatter upper [($show-string v) (string-upcase v)])
-    (define-message-formatter writer [($show-datum v) (format-message ($show-string (~s v)))])
-    (parameterize ([current-message-formatter (combine-message-formatters upper writer)])
-      (check-equal? (format-message ($show-string "foo")) "FOO")
-      (check-equal? (format-message ($show-datum  "bar")) "\"BAR\"")
-      (check-exn
-       exn:fail?
-       (λ () (format-message ($other "whatever")))))))
+  (check-equal? (join-lines #:suffix "\n" '("a" "b" "c")) "a\nb\nc"))
