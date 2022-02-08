d59c7e416f099a4ff4f65695571a2262c490df56
Sage Gerard <sage@sagegerard.com>
2022-02-08 17:27:17 -0500

    Update test.rkt

--- test.rkt
+++ test.rkt
@@ -1,36 +1,75 @@
 #lang racket/base
 
-(provide assert test run-tests)
+(provide assert
+         match?
+         test
+         run-all-tests
+         run-tests)
 
 (require racket/function
+         racket/match
+         racket/runtime-path
+         racket/path
          (for-syntax racket/base))
+
+
+(define-runtime-path here ".")
+
+(define failure (gensym))
+(define failure? (curry eq? failure))
+
+
+(struct dynamic-test (procedure)
+  #:property prop:procedure
+  (λ (self)
+    (with-handlers ([failure? void])
+      ((dynamic-test-procedure self)))))
+
+
+(define-syntax-rule (match? value pattern)
+  (match value [pattern #t] [_ #f]))
 
 
 (define-syntax (assert stx)
   (syntax-case stx ()
     [(_ expr)
      (with-syntax ([src (syntax-source stx)] [line (syntax-line stx)])
-       #'(or expr (halt src line 'expr)))]))
+       #'(dynamic-assert src line expr 'expr))]))
 
 
-(define (halt src line v)
-  (raise (exn:fail:user (format "~a:~a: ~s" src line v)
-                        (current-continuation-marks))))
+(define (dynamic-assert src line result expr-datum)
+  (printf "~a (~a, line ~a): ~s~n"
+          (if result "pass" "fail")
+          (and src (file-name-from-path src))
+          line
+          expr-datum)
+  (unless result (raise failure)))
 
 
 (define (run-tests module-path)
-  (define module-path*
-    `(submod ,module-path test))
   (define-values (provided _)
-    (module->exports module-path*))
+    (module->exports module-path))
   (for* ([provisions (in-list provided)]
          [has-identifier (in-list (cdr provisions))])
-    (define bound (dynamic-require module-path* (car has-identifier)))
-    (when (and (procedure? bound)
-               (procedure-arity-includes? bound 0))
+    (define bound (dynamic-require module-path (car has-identifier)))
+    (when (dynamic-test? bound)
       (bound))))
+
+
+(define (run-all-tests)
+  (parameterize ([current-directory here])
+    (for ([file-path (in-directory)]
+          #:when (regexp-match? #px"\\.rkt$" file-path))
+      (define module-path
+        `(submod ,(format "~a" (find-relative-path (current-directory) file-path)) test))
+      (define instantiation-status
+        (with-handlers ([values values])
+          (dynamic-require module-path #f)))
+      (unless (exn? instantiation-status)
+        (writeln module-path)
+        (run-tests module-path)))))
 
 
 (define-syntax-rule (test id . xs)
   (begin (provide id)
-         (define id (compose void (thunk . xs)))))
+         (define id (dynamic-test (λ () . xs)))))
