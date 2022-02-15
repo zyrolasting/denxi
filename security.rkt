92198c37346cf1c229c865ade5e92bc7bd304b58
Sage Gerard <sage@sagegerard.com>
2022-02-14 23:49:16 -0500

    Rewrite security.rkt

--- security.rkt
+++ security.rkt
@@ -1,358 +1,51 @@
 #lang racket/base
 
-; This module acts as an inflection point between a privileged runtime
-; and a less-privileged runtime.  This is not a substitute for
-; OS-level security.
+(provide call-with-time-limit
+         call-with-memory-limit
+         call-with-environment-variable-subset)
 
-(require (only-in net/url-connect
-                  current-https-protocol)
-         racket/contract
-         racket/exn
-         racket/function
-         racket/list
-         openssl
-         "codec.rkt"
-         "integrity.rkt"
-         "state.rkt"
-         "message.rkt"
-         "path.rkt"
-         "port.rkt")
+(require racket/function)
 
-(provide (struct-out $restrict)
-         (struct-out $restrict:operation)
-         (struct-out $restrict:budget)
-         (contract-out
-          [restrict
-           (-> procedure?
-               (-> any)
-               #:memory-limit (>=/c 0)
-               #:time-limit (>=/c 0)
-               #:trusted-executables (listof well-formed-integrity?)
-               #:allowed-envvars (listof (or/c bytes-environment-variable-name? string?))
-               #:trust-unverified-host? boolean?
-               #:trust-any-executable? boolean?
-               #:trust-host-executables (listof path-string?)
-               #:trust-certificates (listof path-string?)
-               #:writeable-directories (listof path-string?)
-               #:gc-period (>/c 0)
-               #:name string?)]))
+(define (call-with-time-limit time-limit timed-out continue)
+  (define return-values
+    values)
 
-(define-message $restrict (name))
-(define-message $restrict:operation $restrict (reporting-guard summary args))
-(define-message $restrict:budget $restrict (kind amount))
+  (define timed-thread
+    (thread (thunk (call-with-values continue
+                                     (λ A (set! return-values (λ () (apply values A))))))))
+
+  (sync/enable-break (handle-evt (alarm-evt (+ (current-inexact-milliseconds)
+                                               (* time-limit 1000)))
+                                 (λ _
+                                   (kill-thread timed-thread)
+                                   (timed-out)))
+                     (handle-evt (thread-dead-evt timed-thread)
+                                 (λ _
+                                   (return-values)))))
 
 
-(define (restrict halt
-                  proc
-                  #:memory-limit memory-limit
-                  #:time-limit time-limit
-                  #:trusted-executables trusted-executables
-                  #:allowed-envvars allowed-envvars
-                  #:trust-unverified-host? trust-unverified-host?
-                  #:trust-any-executable? trust-any-executable?
-                  #:implicitly-trusted-host-executables implicitly-trusted-host-executables
-                  #:trust-certificates trust-certificates
-                  #:writeable-directories writeable-directories
-                  #:gc-period gc-period
-                  #:name name)
-  (call-with-managed-thread (make-gc-thread gc-period)
-                            (λ _
-                              ; The `plan' mechanic allows the two threads at play
-                              ; to decide how to apply `halt'.
-                              (define finish halt)
-                              (define (plan c m)
-                                (set! finish (λ () (halt c m))))
-
-                              (define security-guard
-                                (make-custom-security-guard
-                                 #:name name
-                                 #:trust-any-executable? trust-any-executable?
-                                 #:trust-executables trusted-executables
-                                 #:writeable-directories writeable-directories
-                                 #:trust-host-executables implicitly-trusted-host-executables))
-
-                              (call-with-managed-thread
-                               (make-worker-thread name
-                                                   memory-limit
-                                                   security-guard
-                                                   (make-envvar-subset allowed-envvars)
-                                                   plan
-                                                   trust-unverified-host?
-                                                   trust-certificates
-                                                   proc)
-                               (λ (worker-thread)
-                                 (unless (sync/timeout time-limit worker-thread)
-                                   (plan 1 ($restrict:budget name 'time time-limit)))
-                                 (finish))))))
+(define (call-with-memory-limit limit continue)
+  (define stop-cust #f)
+  (dynamic-wind (λ ()
+                  (set! stop-cust (make-custodian))
+                  (custodian-limit-memory stop-cust limit stop-cust))
+                (λ ()
+                  (parameterize ([current-custodian stop-cust])
+                    (continue)))
+                (λ ()
+                  (when (custodian? stop-cust)
+                    (custodian-shutdown-all stop-cust)))))
 
 
-;-------------------------------------------------------------------------------
-; Control
-
-(define (make-worker-thread name
-                            memory-limit
-                            security-guard
-                            envvars plan
-                            trust-unverified-host?
-                            trust-certificates
-                            proc)
-  (thread
-   (λ ()
-     (call-with-custom-custodian memory-limit
-                                 (λ ()
-                                   (with-handlers ([exn:fail:out-of-memory?
-                                                    (λ _ (plan 1 ($restrict:budget name 'space memory-limit)))]
-                                                   [exn?
-                                                    (λ (e) (plan 1 ($show-string (exn->string e))))])
-                                     (parameterize ([current-environment-variables envvars]
-                                                    [current-https-protocol
-                                                     (make-ssl-context trust-certificates
-                                                                       trust-unverified-host?)]
-                                                    [current-security-guard security-guard])
-                                       (call-with-values (λ () (call/cc proc))
-                                                         plan))))))))
-
-
-(define (make-ssl-context trust-certificates trust-unverified-host?)
-  (define ctx (ssl-make-client-context 'auto))
-  (unless trust-unverified-host?
-    (ssl-load-default-verify-sources! ctx)
-    (for ([source (in-list trust-certificates)])
-      (ssl-load-verify-source! ctx source))
-    (ssl-set-verify! ctx #t)
-    (ssl-set-verify-hostname! ctx #t)
-    ; No weak cipher suites
-    (ssl-set-ciphers! ctx "DEFAULT:!aNULL:!eNULL:!LOW:!EXPORT:!SSLv2")
-    ; Seal context so further changes cannot weaken it
-    (ssl-seal-context! ctx))
-  ctx)
-
-;-------------------------------------------------------------------------------
-; Memory management
-
-(define (make-custom-custodian memory-limit-mb)
-  (let ([stop-cust (make-custodian)])
-    (if (or (equal? memory-limit-mb +inf.0)
-            (not (custodian-memory-accounting-available?)))
-        stop-cust
-        (begin (custodian-limit-memory stop-cust
-                                       (mebibytes->bytes memory-limit-mb)
-                                       stop-cust)
-               stop-cust))))
-
-
-(define (call-with-custom-custodian memory-limit-mb proc)
-  (let ([cust (make-custom-custodian memory-limit-mb)])
-    (dynamic-wind void
-                  (λ ()
-                    (parameterize ([current-custodian cust])
-                      (proc)))
-                  (λ ()
-                    (custodian-shutdown-all cust)))))
-
-
-(define (make-gc-thread period)
-  (thread
-   (λ ()
-     (let loop ()
-       (sync/timeout period never-evt)
-       (collect-garbage)
-       (loop)))))
-
-
-(define (call-with-managed-thread th proc)
-  (dynamic-wind void
-                (λ () (proc th))
-                (λ () (kill-thread th))))
-
-
-(define (make-envvar-subset allowed [input-set (current-environment-variables)])
-  (define subset-names (remove-duplicates (map coerce-bytes (cons "PATH" allowed))))
-  (apply make-environment-variables
-         (for/fold ([mappings null])
-                   ([name (in-list subset-names)])
-           (define value (environment-variables-ref input-set name))
-           (if value
-               (cons name
-                     (cons (environment-variables-ref input-set name)
-                           mappings))
-               mappings))))
-
-
-;-------------------------------------------------------------------------------
-; Security guards
-
-(define (make-custom-security-guard . args)
-  (apply make-security-guard (current-security-guard) args))
-
-
-(define (make-file-guard #:trust-any-executable? trust-any-executable?
-                         #:trust-host-executables trust-host-executables
-                         #:trust-executables trust-executables
-                         #:writeable-directories writeable-directories
-                         name)
-  (let ([trust-executable? (make-executable-trust-predicate trust-any-executable?
-                                                            trust-executables
-                                                            (cons "openssl" trust-host-executables))])
-    (λ (sym path-or-#f ops)
-      (define (check-destructive-op op path)
-        (define test (curry path-prefix? (normalize-path path)))
-        (unless (ormap test writeable-directories)
-          (raise ($restrict:operation name 'file op (list sym path-or-#f ops)))))
-
-      (when path-or-#f
-        (cond [(member 'execute ops)
-               (unless (trust-executable? path-or-#f)
-                 (raise ($restrict:operation name 'file 'blocked-execute (list sym path-or-#f ops))))]
-
-              [(member 'write ops)
-               (check-destructive-op 'blocked-write path-or-#f)]
-
-              [(member 'delete ops)
-               (check-destructive-op 'blocked-delete path-or-#f)])))))
-
-
-(define (make-executable-trust-predicate trust-any-executable? trust-executables trust-host-executables [find find-executable-path])
-  (define trust-by-integrity? (bind-trust-list trust-executables))
-  (define host-executable-paths (map normalize-path (filter-map find trust-host-executables)))
-  (λ (path)
-    (let ([normalized (normalize-path path)])
-      (or trust-any-executable?
-          (and (member normalized host-executable-paths) #t)
-          (trust-by-integrity? normalized)))))
-
-
-(define (make-network-guard name)
-  (λ (sym hostname-or-#f port-or-#f client-or-server)
-    (unless hostname-or-#f
-      (raise ($restrict:operation name
-                                  'network
-                                  'blocked-listen
-                                  (list sym hostname-or-#f port-or-#f client-or-server))))))
-
-
-(define (make-link-guard name workspace)
-  (λ (op link-path maybe-complete-target-path)
-    (define target-path (simple-form-path maybe-complete-target-path))
-    (unless (path-prefix? target-path workspace)
-      (raise ($restrict:operation name
-                                  'link
-                                  'blocked-link
-                                  (list op link-path target-path))))))
-
-
-
-(module+ test
-  (require racket/match
-           racket/runtime-path
-           rackunit
-           "file.rkt")
-
-  (define-runtime-path here ".")
-
-  (define (station-security-guard f n l continue)
-    (parameterize ([current-security-guard
-                    (make-security-guard (current-security-guard) f n l)])
-      (continue)))
-
-  (define (station-file-guard f p)
-    (station-security-guard f void void p))
-
-  (define (station-network-guard n p)
-    (station-security-guard void n void p))
-
-  (define (station-link-guard l p)
-    (station-security-guard void void l p))
-
-  (test-case "Terminate thread at end of procedure"
-    (define useless (thread (λ () (sync never-evt))))
-    (call-with-managed-thread useless
-                              (λ (th)
-                                (check-eq? th useless)
-                                (check-pred thread-running? th)))
-    (check-pred thread-dead? useless))
-
-  (test-exn "Restrict listening for network connections"
-            (λ (e)
-              (match e
-                [($restrict:operation "_" 'network 'blocked-listen _) #t]
-                [_ #f]))
-            (λ ()
-              (station-network-guard (make-network-guard "_")
-                                     (λ ()
-                                       (local-require racket/tcp)
-                                       (tcp-listen 9000)))))
-
-  (test-case "Restrict link creation to files in workspaces"
-    (call-with-temporary-directory
-     (λ (dir)
-       (station-link-guard
-        (make-link-guard "_" dir)
-        (λ ()
-          (define link-path (build-path dir "link"))
-          (define target-path (build-path dir "target"))
-          (define (ln t)
-            (make-file-or-directory-link t link-path))
-
-          ; The security guard only objects if a symlink points
-          ; outside to a non-child of `dir`.
-          (check-exn
-           (λ (e)
-             (match e
-               [($restrict:operation "_" 'link 'blocked-link _) #t]
-               [_ #f]))
-           (λ ()
-             ; Using two 'up arguments because the guard sees only
-             ; a trailing slash difference for some reason.
-             (ln (build-path target-path 'up 'up))))
-
-          (check-not-exn
-           (λ () (ln target-path))))))))
-
-
-  (test-case "Build executable trust profiles"
-    (call-with-snake-oil-chf-trust
-     (λ ()
-       (define my-file "security.rkt")
-       (define my-path (build-path here my-file))
-       (define other-path (build-path here "main.rkt"))
-
-       (define trusts-anything
-         (make-executable-trust-predicate #t '() '()))
-
-       (define trusts-nothing
-         (make-executable-trust-predicate #f '() '()))
-
-       (define trusts-exact-things
-         (make-executable-trust-predicate #f
-                                          (list (integrity (get-default-chf)
-                                                           (make-digest my-path)))
-                                          '()))
-
-       (define trusts-hosted-things
-         (make-executable-trust-predicate #f '() '("security.rkt") (λ (p) my-path)))
-
-       (check-true  (trusts-anything my-path))
-       (check-true  (trusts-anything other-path))
-
-       (check-false (trusts-nothing my-path))
-       (check-false (trusts-nothing other-path))
-
-       (check-true  (trusts-exact-things my-path))
-       (check-false (trusts-exact-things other-path))
-
-       (check-true  (trusts-hosted-things my-path))
-       (check-false (trusts-hosted-things other-path)))))
-
-
-  (test-case "Make envvar subset"
-    (define subset
-      (make-envvar-subset
-       '(#"bar")
-       (make-environment-variables #"foo" #"1"
-                                   #"bar" #"2"
-                                   #"baz" #"3")))
-
-    (check-false (environment-variables-ref subset #"foo"))
-    (check-false (environment-variables-ref subset #"baz"))
-    (check-equal? (environment-variables-ref subset #"bar") #"2")))
+(define (call-with-environment-variable-subset input-set allowed continue)
+  (parameterize ([current-environment-variables
+                  (apply make-environment-variables
+                         (for/fold ([mappings null])
+                                   ([name (in-list allowed)])
+                           (define value (environment-variables-ref input-set name))
+                           (if value
+                               (cons name
+                                     (cons (environment-variables-ref input-set name)
+                                           mappings))
+                               mappings)))])
+    (continue)))
