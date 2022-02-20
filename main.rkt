d80635f38445369275dcb4211e433d95ae9eaa1f
Sage Gerard <sage@sagegerard.com>
2022-02-20 18:48:54 -0500

    Shrink denxi/main

--- main.rkt
+++ main.rkt
@@ -6,12 +6,9 @@
          (provide (all-from-out . x))))
 
 (reprovide "dependent.rkt"
-           "file.rkt"
            "io.rkt"
            "machine.rkt"
            "message.rkt"
            "monad.rkt"
-           "peer.rkt"
            "port.rkt"
-           "security.rkt"
-           "tcp.rkt")
+           "security.rkt")
