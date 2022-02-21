a4b69a2974a77fa4871f82a5907c4296c6be1406
Sage Gerard <sage@sagegerard.com>
2022-02-20 19:46:51 -0500

    Support in-place linking

--- ship.rkt
+++ ship.rkt
@@ -21,20 +21,26 @@
 
 (module+ main
   (require racket/cmdline)
-
-  (command-line #:args ([edition "unnamed"])
-                (define dirname
-                  (format "denxi-~a-~a" edition (current-seconds)))
-
-                (pretty-write #:newline? #t
-                              `(module setup racket/base
-                                 (require racket/runtime-path)
-                                 (define-runtime-path extraction/ ,dirname)
-                                 ,(firework dirname
-                                            (for/hash ([path (in-directory source-code/)]
-                                                       #:when (and (not (regexp-match? #px"\\.git" path))
-                                                                   (file-exists? path)))
-                                              (values (find-relative-path source-code/ path)
-                                                      (cons #o440 (file->bytes path)))))
-                                 (require (submod 'packed main))
-                                 ((dynamic-require (build-path extraction/ "setup.rkt") 'link))))))
+  (define in-place? #f)
+  (command-line
+   #:once-each
+   [("-i" "--in-place")
+    "Use existing source code"
+    (set! in-place? #t)]
+   #:args ([edition "unnamed"])
+   (define dirname
+     (format "denxi-~a-~a" edition (current-seconds)))
+   (if in-place?
+       (link-denxi-collection)
+       (pretty-write #:newline? #t
+                     `(module setup racket/base
+                        (require racket/runtime-path)
+                        (define-runtime-path extraction/ ,dirname)
+                        ,(firework dirname
+                                   (for/hash ([path (in-directory source-code/)]
+                                              #:when (and (not (regexp-match? #px"\\.git" path))
+                                                          (file-exists? path)))
+                                     (values (find-relative-path source-code/ path)
+                                             (cons #o440 (file->bytes path)))))
+                        (require (submod 'packed main))
+                        ((dynamic-require (build-path extraction/ "setup.rkt") 'link)))))))
