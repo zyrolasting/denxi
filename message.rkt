d70855a41ffd1682557a71658e431694cc5a1563
Sage Gerard <sage@sagegerard.com>
2022-02-26 12:39:13 -0500

    Rewrite message.rkt into contracted variant (Thanks to SamPh)

--- message.rkt
+++ message.rkt
@@ -4,53 +4,56 @@
 ; machine-readable documents.
 
 (provide (struct-out $message)
-         (struct-out $show-datum)
-         (struct-out $show-string)
-         default-message-formatter
          define-message)
 
 (require racket/contract
          (for-syntax racket/base
+                     racket/contract
                      racket/syntax))
+
+(struct $message () #:prefab)
+(define-syntax $message-fields/c null)
 
 
 (define-syntax (define-message stx)
   (syntax-case stx ()
     [(_ id super-id ([field contract] ...))
      (andmap identifier? (syntax-e #'(id super-id field ...)))
-     (with-syntax ([id/c (format-id #'id "~a/c" #'id)])
-       #'(begin (struct id super-id (field ...) #:prefab)
-                (define id/c (invariant-assertion flat-contract? (struct/c id contract ...)))))]
+     (with-syntax* ([id/c (format-id #'id "~a/c" #'id)]
+                    [id-fields/c (format-id #'id "~a-fields/c" #'id)]
+                    [(super-id-fields/c ...)
+                     (syntax-local-value
+                      (format-id #'super-id "~a-fields/c" #'super-id))])
+       #'(begin
+           (struct id super-id (field ...) #:prefab)
+           (define id/c
+             (invariant-assertion
+              flat-contract? (struct/c id super-id-fields/c ... contract ...)))
+           (define-syntax id-fields/c
+             #'(contract ...))))]
     [(_ id (f+c ...))
      #'(define-message id $message (f+c ...))]))
-
-
-(struct $message () #:prefab)
-(define-message $show-datum  ([value any/c]))
-(define-message $show-string ([value string?]))
-
-
-(define (default-message-formatter v)
-  (cond [($show-string/c v) ($show-string-value v)]
-        [($show-datum/c v) (format "~s" ($show-datum-value v))]
-        [else (format "~s" v)]))
 
 
 (module+ test
   (require "test.rkt")
 
-  (define-message $foo ([a real?] [b real?] [c real?]))
+  (define-message $foo ([a real?]))
+  (define-message $subfoo $foo ([a string?]))
+  (define valid ($subfoo 1 "b"))
+  (define invalid ($subfoo 1 2))
 
   (test message-structure
-        (define foo-inst ($foo 1 2 3))
-        (assert ($foo? foo-inst))
-        (assert (equal? ($foo-a foo-inst) 1))
-        (assert (equal? ($foo-b foo-inst) 2))
-        (assert (equal? ($foo-c foo-inst) 3)))
+        (assert ($foo? valid))
+        (assert ($subfoo? valid))
+        (assert (equal? ($foo-a valid) 1))
+        (assert (equal? ($subfoo-a valid) "b")))
 
   (test message-contracts
-        (assert ($foo/c ($foo 1 2 3)))
-        (assert (not ($foo/c ($foo "1" 2 3))))
+        (assert ($foo/c valid))
+        (assert ($subfoo/c valid))
+        (assert ($foo/c invalid))
+        (assert (not ($subfoo/c invalid)))
         (with-handlers ([exn:fail:contract?
                          (λ (e) (assert 'define-message-blocked))])
           (define-message $bar ([a (-> any)]))
